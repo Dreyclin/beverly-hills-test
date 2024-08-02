3019db14d01f03e62346bd2f6e848d739d2b10eb
Maxim Sur <makarka16@gmail.com>
2024-08-02 12:24:49 +0300

    Add: burger menu

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const glide = new Glide('.glide', {
-        type: 'carousel', // тип слайдера (может быть 'carousel' или 'slider')
-        perView: 1 // количество отображаемых слайдов одновременно
-    }).mount();
-});
+const glide = new Glide('.glide', {
+    type: 'carousel',
+    perView: 1
+}).mount();
+
+$('#menu-toggler').on('click', () => {
+    $('.nav-container').toggleClass('hidden');
+    $('header').toggleClass('zero-margin');
+})
+
+$('#tours-toggler').on('click', () => {
+    $('.tours-list').toggleClass('hidden');
+})
+
+$('#offer-toggler').on('click', () => {
+    $('.offer-list').toggleClass('hidden');
+})
